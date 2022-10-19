231f64e825fa03a33f86f39f14e3bbf30676bf9c
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-10-19 19:20:57 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketСost = 14;
+        int costpermile = 20;
+        if (ticketСost / costpermile <=- 20) {
+            System.out.println("Колличество ваших миль:" + ticketСost / costpermile);
+        } else {
+            System.out.println("Стоимость билета не может быть менее 20 рублей");
+        }
+
+    }
+}
