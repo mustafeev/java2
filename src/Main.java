76a240b1740eb8e972c921796e7d49248b9dc7b9
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-10-19 20:03:37 +0400

    secondary

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketСost = 14;
+        int ticketСost = 100;
         int costpermile = 20;
-        if (ticketСost / costpermile <=- 20) {
+        if (ticketСost / costpermile >= 1) {
             System.out.println("Колличество ваших миль:" + ticketСost / costpermile);
         } else {
             System.out.println("Стоимость билета не может быть менее 20 рублей");
